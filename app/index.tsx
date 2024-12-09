12c4d77f5de530d73ce0440d0757cdb12c9d5ebc
Damian Kadziela <damiankadziela4@gmail.com>
2024-12-09 14:18:47 +0100

    firebase setup && android prebuild

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on yousssr app!</Text>
+    <View className='flex-1 flex justify-center items-center'>
+      <Text>Hi there!</Text>
+
       <StatusBar style='auto' />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
